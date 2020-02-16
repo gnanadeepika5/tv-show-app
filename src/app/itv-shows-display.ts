66432dc20c0b6a22bc43eaa8aab22a8e9e8bb3a6
banudemir <60566114+banudemir@users.noreply.github.com>
2020-02-15 16:54:31 -0800

    Adding first interface definition for ITvShowDisplay

---
+++ src/app/itv-shows-display.ts
@@ -0,0 +1,13 @@
+export interface ITvShowsDisplay {
+  name: string,
+  language: string,
+  schedule: {
+    time: number,
+    days: number
+  },
+  rating: number,
+  image: string,
+  summary: string,
+  networkname: string
+
+}
--- src/app/tv-shows-display/tv-shows-display.component.ts
+++ src/app/tv-shows-display/tv-shows-display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ITvShowsDisplay } from '../itv-shows-display';
 
 @Component({
   selector: 'app-tv-shows-display',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tv-shows-display.component.css']
 })
 export class TvShowsDisplayComponent implements OnInit {
-currentdata:
-  constructor() { }
+  currentdata: ITvShowsDisplay
+  constructor() { 
+    this.currentdata.image ="test location",
+    this.currentdata.language = "EN",
+    this.currentdata.networkname = "ABC",
+    this.currentdata.rating = 4.5
+    this.currentdata.summary = "This is a test tvshow",
+    this.currentdata.schedule.days = 2,
+    this.currentdata.schedule.time = 17
+  }
 
   ngOnInit(): void {
   }
