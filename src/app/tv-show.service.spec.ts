2c5cf6e5eac011581f5a9f68b9627381c1e3914a
banudemir <60566114+banudemir@users.noreply.github.com>
2020-02-22 14:18:45 -0800

    Initial schedule change

--- src/app/tv-show.service.spec.ts
+++ src/app/tv-show.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { TvShowService } from './tv-show.service';
 
 describe('TvShowService', () => {
   let service: TvShowService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(TvShowService);
   });
 
--- src/app/tv-shows-display/tv-shows-display.component.spec.ts
+++ src/app/tv-shows-display/tv-shows-display.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { TvShowsDisplayComponent } from './tv-shows-display.component';
 
 describe('TvShowsDisplayComponent', () => {
@@ -8,7 +8,8 @@ describe('TvShowsDisplayComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TvShowsDisplayComponent ]
+      declarations: [ TvShowsDisplayComponent ],
+      imports: [HttpClientModule]
     })
     .compileComponents();
   }));
