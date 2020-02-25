e8b057e4481510effca984c87d07d4455425966f
gnanadeepika5 <60079729+gnanadeepika5@users.noreply.github.com>
2020-02-25 00:05:56 -0800

    removed hard coded showname in tv shoes display component
    removed hard coded showname in tv shoes display component

--- src/app/tv-shows-display/tv-shows-display.component.ts
+++ src/app/tv-shows-display/tv-shows-display.component.ts
@@ -14,7 +14,7 @@ export class TvShowsDisplayComponent implements OnInit {
   constructor(private tvshowservice: TvShowService) { }
   
   ngOnInit(): void {
-    this.tvshowservice.getShowDetails('marvel').subscribe(data => this.currentdata = data);
+    
   }
 
 }
